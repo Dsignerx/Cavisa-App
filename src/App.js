3649a7739b0158f415b322da9d6f3b6f51d96b59
Juan Osorio <andesignerx@gmail.com>
2024-05-06 17:52:07 -0500

    Caldi-App
    Caldi App es una aplicación web desarrollada en React Js, consiste en brindar diversos módulos de ocio y economía para los traders que buscan pasar el tiempo mientras ven algún tipo de contenido en alguno de los canales de tv a nivel mundial dentro del app.

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ const App = () => {
   const cambios=[
     {
       moneda: "Cantidad",
-      cambio:0,
+      cambio: 0,
     },{
       moneda: "Peso argentino",
-      cambio:4.46,
+      cambio:0.22,
     },{
       moneda: "Euro",
-      cambio:4000,
+      cambio:0.00024,
     },{
       moneda: "Peso mexicano",
-      cambio:227.13,
+      cambio:0.0043,
     },{
       moneda: "Dólar",
-      cambio:3800
+      cambio:0.00025
     }
   ]
    
@@ -39,22 +39,52 @@ const App = () => {
    setResultado1(q.target.value*cambios[1].cambio)
    setResultado2(q.target.value*cambios[2].cambio)
    setResultado3(q.target.value*cambios[3].cambio)
-   setResultado4
-   (q.target.value*cambios[4].cambio)
+   setResultado4(q.target.value*cambios[4].cambio)
   }
+
+ 
+  
 
 
   return (
-    <>
     
+<div>
+
+    <div>
+     
+     <nav  className="navbar">
+     <lo><button>opcion1</button></lo>
+     <lo><button>opcion2</button></lo>
+     <lo><button>opcion3</button></lo>
+     
+     </nav>
+    
+    
+
+    <section className="title1" >
+    <div className='video'>
+    <iframe width="920px" height="680px" src="https://www.youtube.com/embed/aO2ByzUumaQ?si=vSr7RE3Xsn6wHxQm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>  
+      <h1>Calculador de Divisas </h1>
+      
+    </section> 
+
+    </div>
+  
+
     <div className='caja1'>
 
-    <label htmlFor="u0">{cambios[0].moneda}</label>
-        <input id="u0" type="number" value={cantidad} onChange={modificar}/> <br></br>
+    <div className='ban-g' name='Bner-v-1'>
+      <h4>BannerGoogle</h4>
+    </div>  
 
+     <div className='calculador'>
+    <label  htmlFor="u0">{cambios[0].moneda}</label>
+        <input id="u0" type="number" value={cantidad} onChange={modificar}/> <br></br>
+        <div>
     <label htmlFor="u1">{cambios[1].moneda}</label>
         <input id="u1" type="number" value={resultado1} readOnly/> <br></br>
-
+        </div> 
     <label htmlFor="u2">{cambios[2].moneda}</label>
         <input id="u2" type="number" value={resultado2} readOnly/> <br></br>
 
@@ -63,15 +93,46 @@ const App = () => {
 
     <label htmlFor="u4">{cambios[4].moneda}r</label>
         <input id="u4" type="number" value={resultado4} readOnly/> 
-
+     </div>
 
     </div>
     
+    <footer  className='footer1'>
+
+      <form method='get' action='<a href="mailto:andesigner@gmail.com?Subject=Correo%20de%20prueba%20del%20mail"></a>' className='form'>
+       <div className='form-n'>
+       <label>Nombre</label> <br></br>
+      <input className='put-space' id='f0' type="text"/>
+      </div> 
+       <div className='form-n'>
+       <label>Email</label> <br></br>
+      <input type='email' className='put-space' id='f1' />
+      </div> 
+       <div className='form-n'>
+       <label>Telefono</label> <br></br>
+      <input type='number' className='put-space' id='f2' />
+      </div> 
+       <div className='form-n'>
+       <label>Observación</label> <br></br>
+      <textarea className='put-space' id='f3' type="text"/>
+      </div>
+      <div>
+      <br></br>
+      <button type='submit'>hello</button>
+      </div>
+      </form>
+
+      
+
+       
+
+
+     <a className="footer" href="https://www.juandev.site">By Juandev</a>
+
+    </footer>
     
+  </div>
     
-    
-    
-    </>
   )
 }
 
